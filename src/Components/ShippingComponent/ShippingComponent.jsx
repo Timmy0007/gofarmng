f9c80dac137ec4e90352e3eb8dd093682d7f75c4
Kazeem Bankole <tunjibankole@gmail.com>
2022-11-08 10:48:03 +0100

    created the shipping component

---
+++ src/Components/ShippingComponent/ShippingComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ShippingComponent = () => {
+  return (
+    <div className=''>
+        <h1>ShippingComponent </h1>
+        <div className=''>
+            <h3>Personal Details</h3>
+
+        </div>
+
+    </div>
+
+  )
+}
+
+export default ShippingComponent

313b592a3f9923a734ba3c8ff7dbb35d49d6c5da
Yusuff Hammed <yusuffhammed43@gmail.com>
2022-11-18 13:57:34 -0800

    worked on the product page of gofarmng

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import ShippingComponent from './Components/ShippingComponent/ShippingComponent';
-
+import Header from "./Components/Header"
+import ProductPage from "./Pages/ProductsPage/ProductPage"
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <ProductPage/>
       <ShippingComponent />
+      
     </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Header.css"
+import bell from "../assets/Bell.png"
+import cart from "../assets/cart.png"
+
+const Header = () => {
+  return (
+    <div className='header-content'>
+        <h3>GoFarmNG</h3>
+        <div className="select">
+            <select name="" id="">
+                <option value="All-Categories">All Categories</option>
+                <option value="Cereal">Cereal Crops</option>
+                <option value="Fruits">Fruits</option>
+                <option value="Tubers">Tubers</option>
+                <option value="Jiuices">Juices</option>
+                <option value="Bakery">Bakery</option>
+                <option value="Spices">Spices</option>
+                <option value="Seafoods">Sea Foods</option>
+                <option value="Vegetables">Vegetables</option>
+                <option value="dairy">Dairy</option>
+                <option value="Beverages">Beverages</option>
+                <option value="Meat">Meat</option>
+            </select>
+            <input className='srch' type="search"placeholder="  | Search Products..."/>
+            <i class="fa fa-search"></i>
+           
+        </div>
+        <div className="icons">
+            <img src={bell} alt=""/>
+            <img src={cart} alt=""/>
+           
+           
+        </div>
+    </div>
+  )
+}
+
+export default Header

187ffc43e32b77c4d6234db9b3a6eb113b570594
Timmy007 <tim4life00@gmail.com>
2022-11-30 00:45:11 -0800

    Api succesfully done

--- src/Pages/Forgotpass/forgot.jsx
+++ src/Pages/Forgotpass/forgot.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import './forgot.css'
 import { Link } from 'react-router-dom'
+import { useState } from "react";
 
-
+async function forgot(item) {
+  return fetch('https://gofarmng.herokuapp.com/api/forgotpassword', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(data => data.json())
+ }
 
 function Forgot() {
+  const handleChange = (e)=>{
+    setEmail(e.target.value);
+  }
+
+  const [email, setEmail]= useState('')
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await forgot({
+      email
+    })
+    localStorage.setItem('user', JSON.stringify(response['user']));
+  };
+
   return (
     <div className='Forgot'>
       <div className='forgot1'>
-      <form>
+      <form onSubmit={handleSubmit}>
       <label>
       <p className='email'>Email Address</p>
-      <input type="email" id='emailf' name="email" placeholder="tim@gmail.com" required/>
+      <input type="email" id='emailf' name="email" placeholder="tim@gmail.com" required onChange={handleChange}/>
       </label>
       <p className='resetpass'>A password reset link would be sent to your email </p>
       <button className='signupb'>Continue</button>

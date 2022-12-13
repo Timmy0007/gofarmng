b0821e51afa87ecd2d713fdfc4ef111dab2f86e1
lolapoland <agboolaf268@gmail.com>
2022-12-13 08:21:50 +0100

    SubscribeNewsLetter

---
+++ src/Components/SubscribeNewsLetter/SubscribeNewsLetter.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./SubscribeNewsLetter.css";
+import icon from "../images/icon.svg";
+
+function SubscribeNewsLetter() {
+  const subscribeNewsEvent = (e) => {
+    e.preventDefault();
+  };
+  const emailValidityChecker = (email) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+      return true;
+    }
+    alert("You have entered an invalid email address!");
+    return false;
+  };
+  return (
+    <div className="subscribe-container">
+      <div className="subscribe-article">
+        <h3 className="subscribe-header">Subscribe to our newsletter</h3>
+        <p className="subscribe-paragraph">
+          Subscribe to our newsletter to recieve exclusive updates and news from
+          us.
+        </p>
+        <div className="input-button">
+          <input
+            className="subscribe-input"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email address"
+            required
+            onChange={emailValidityChecker}
+          />
+
+          <button className="subscribe-button" onClick={subscribeNewsEvent}>
+            Subscribe
+          </button>
+        </div>
+      </div>
+      <div className="subscribe-icon">
+        <img src={icon} alt="" />
+      </div>
+    </div>
+  );
+}
+export default SubscribeNewsLetter;
